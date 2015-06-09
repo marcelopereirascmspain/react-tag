374751a2d4d9a96a295a131bf71639ff7cd652a0
marcelopereirascmspain <marcelo.pereira@AFE0804061.local>
2015-06-09 18:35:27 +0200

    Add theming capabilities.

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,42 @@
 import React from "react";
 import Tag from "../src/index";
 
-React.render(<Tag text="this is a tag" />, document.getElementById("main"));
+var segundamanoTagTheme = {
+  tag: {
+    backgroundColor: "#f90"
+  },
+  tag__tip: {
+    borderRightColor: "#f90"
+  }
+};
+
+var successTagTheme = {
+  tag: {
+    backgroundColor: "green"
+  },
+  tag__tip: {
+    borderRightColor: "green"
+  }
+};
+
+class SuccessTag extends React.Component {
+  render() {
+    return (
+      <Tag {...this.props} theme={successTagTheme} />
+    );
+  }
+}
+
+class Docs extends React.Component {
+  render() {
+    return (
+      <div>
+        <Tag text="this is a default tag" />
+        <SuccessTag text="this is a wrapped tag with a default theme" />
+        <Tag text="this is a tag with an overriden theme" theme={segundamanoTagTheme} />
+      </div>
+    )
+  }
+}
+
+React.render(<Docs />, document.getElementById("main"));
--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
 import React from "react";
+import merge from "lodash/object/merge";
+
+const backgroundColor = "#aaa";
+const tipWidth = 12;
+
+const styles = {
+  tag: {
+    backgroundColor: backgroundColor,
+    color: "#fff",
+    padding: 9,
+    paddingRight: 32,
+    lineHeight: 1,
+    fontSize: 12,
+    borderRadius: "0 2px 2px 0",
+    position: "relative",
+    marginLeft: tipWidth
+  },
+  tag__tip: {
+    border: "16px solid transparent",
+    borderLeft: 0,
+    borderRight: `${tipWidth}px solid transparent`,
+    borderRightColor: backgroundColor,
+    width: 0,
+    display: "block",
+    position: "absolute",
+    left: -tipWidth,
+    top: 0
+  },
+  tag__remove: {
+    border: 0,
+    color: "#fff",
+    backgroundColor: "transparent",
+    padding: 8,
+    fontSize: 12,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    cursor: "pointer"
+  }
+};
 
 export default React.createClass({
   render: function () {
+    const style = merge(styles, this.props.theme);
+
     return (
-      <span className="schibsted-tag">
+      <span className="schibsted-tag" style={styles.tag}>
+        <span style={styles.tag__tip}></span>
         {this.props.text}
-        <button className="schibsted-tag__remove">&times;</button>
+        <button className="schibsted-tag__remove" style={styles.tag__remove}>&times;</button>
       </span>
     );
   }
